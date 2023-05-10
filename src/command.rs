3ffaa66cc48545934248e56366aed1684ed4413b
akhmadiy <ahmadulinjs@gmail.com>
2023-05-10 15:07:05 +0500

    add command constructor test

--- src/command.rs
+++ src/command.rs
@@ -1,5 +1,5 @@
 // TODO: implement command option struct
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct CopyCommand {
     source: String,
     dest: String,
@@ -11,8 +11,8 @@ pub struct CopyCommand {
 impl CopyCommand {
     pub fn new(source: &str, dest: &str) -> Self {
         Self {
-            source: String::from(source),
-            dest: String::from(dest),
+            source: (*source).to_string(),
+            dest: (*dest).to_string(),
             is_interactive: false,
             is_recursive: false,
             verbose: false,
@@ -27,5 +27,24 @@ impl CopyCommand {
     }
     fn exec() {
         // need implement execution of copy command
+    }
+}
+
+
+#[cfg(test)]
+mod command_tests {
+    use super::CopyCommand;
+
+    #[test]
+    fn chek_build_new_command() {
+        let com = CopyCommand::new(&String::from("/home/trash"), &String::from("/home/trash/app"));
+
+        assert_eq!(com, CopyCommand {
+            source: String::from("/home/trash"),
+            dest: String::from("/home/trash/app"),
+            is_interactive: false,
+            is_recursive: false,
+            verbose: false,
+        })
     }
 }
